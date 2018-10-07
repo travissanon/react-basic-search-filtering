15c41c3e92d0d1269501f40af65f14e804950a7d
Travis <travis@devmaterial.com>
2018-10-07 10:36:08 -0400

    [add] first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,73 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const people = [
+  {
+    id: 1,
+    first: "Sarah",
+    last: "Johnson",
+    age: 25
+  },
+  {
+    id: 2,
+    first: "Caldwell",
+    last: "Thompson",
+    age: 32
+  },
+  {
+    id: 3,
+    first: "Hart",
+    last: "Maynard",
+    age: 12
+  },
+  {
+    id: 4,
+    first: "Demetrius",
+    last: "McGregor",
+    age: 55
+  },
+]
+
+let searchingFor = (term) => {
+  return function(x) {
+    return x.first.toLowerCase().includes(term.toLowerCase()) || !term;
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: people,
+      term: '',
+    }
+    
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+
+  searchHandler(e) {
+    this.setState({term: e.target.value})
+  }
   render() {
+    const { people, term } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <input type="text"
+            onChange={this.searchHandler}
+            value={term}
+          />
+        </form>
+        {
+          people.filter(searchingFor(term)).map(person =>
+            <div key={ person.id }>
+              <h1>{ person.first }</h1>
+              <h1>{ person.last }</h1>
+              <h3>{ person.last }</h3>
+            </div>
+          )
+        }
       </div>
     );
   }
